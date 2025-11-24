9ed181fb3e659b51a04c6ea56e23aff3eb511b9a
Elam Bravo <bravoelam@gmail.com>
2025-11-24 16:27:49 +0100

    Crear objeto "Coment Constructor" de ImagenPost

--- src/main/java/com/campusdual/Coments.java
+++ src/main/java/com/campusdual/Coments.java
@@ -1,4 +1,16 @@
 package com.campusdual;
 
+import java.util.Date;
+
 public class Coments {
+    private String content;
+    private Date comentDate;
+    private Usuario user;
+
+    public Coments (String content, Usuario user){
+        this.content = content;
+        this.user = user;
+        this.comentDate = new Date();
+    }
+
 }
--- src/main/java/com/campusdual/ImagenPost.java
+++ src/main/java/com/campusdual/ImagenPost.java
@@ -1,6 +1,14 @@
 package com.campusdual;
 
-public class ImagenPost {
+import java.util.Date;
+
+public class ImagenPost extends Post {
 
     private int dimensions;
+
+    public ImagenPost(int dimensions, String title) {
+        this.dimensions = dimensions;
+        this.title = title;
+        this.postDate = new Date();
+    }
 }
