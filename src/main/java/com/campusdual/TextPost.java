e5266492ab9e9284a3d5ad475a5d414aeda2d002
Elam Bravo <bravoelam@gmail.com>
2025-11-24 16:35:26 +0100

    Crear objeto "Video"

--- src/main/java/com/campusdual/TextPost.java
+++ src/main/java/com/campusdual/TextPost.java
@@ -1,7 +1,15 @@
 package com.campusdual;
 
-public class TextPost extends Post {
+import java.util.Date;
 
-   private String comennt;
+public class TextPost extends Post {
+    public TextPost(String comennt, String title) {
+        this.comennt = comennt;
+        this.title = title;
+        this.postDate = new Date();
+    }
+
+    private String comennt;
+
 
 }
--- src/main/java/com/campusdual/VideoPost.java
+++ src/main/java/com/campusdual/VideoPost.java
@@ -1,6 +1,14 @@
 package com.campusdual;
 
-public class VideoPost {
+import java.util.Date;
+
+public class VideoPost extends Post{
+    public VideoPost(String quality, int duration, String title) {
+        this.quality = quality;
+        this.duration = duration;
+        this.title = title;
+        this.postDate = new Date();
+    }
 
     private String quality;
     private int duration;
