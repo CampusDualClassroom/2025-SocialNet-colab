6268cc27e5194fb9085bd0cf94b488e92602365f
Edixon Sánchez <ediiixonsanchezg@gmail.com>
2025-11-26 16:25:35 +0100

    Creación SocialNetwork

--- src/main/java/com/campusdual/SocialNetwork.java
+++ src/main/java/com/campusdual/SocialNetwork.java
@@ -1,4 +1,19 @@
 package com.campusdual;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SocialNetwork {
+    List<Usuario> usuarios = new ArrayList<>();
+    List<Post> post = new ArrayList<>();
+
+    private void createUsers(){
+        String nuevoUsuario = Utils.string("Añade nombre del nuevo usuario");
+        usuarios.add(new Usuario(nuevoUsuario));
+    }
+
+    private Coments createComents(Usuario user){
+        String nuevoComentario = Utils.string("Añade un comentario");
+        return new Coments(nuevoComentario,user);
+    }
 }
